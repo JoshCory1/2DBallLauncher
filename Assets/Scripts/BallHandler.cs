2edf46e5b661f31d1f23130be025f5a7dabbcc6b
Joshua Cory <accounts@keeplooking.net>
2023-09-16 17:14:42 -0600

    Made detach method

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,11 @@ using UnityEngine.InputSystem;
 
 public class BallHandler : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D currentBallRigidbody;
+    [SerializeField] private SpringJoint2D currentBallSpringJoint;
+    [SerializeField] private float delayDuration = 1f;
     private Camera mainCamera;
+    private bool isDraging;
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +21,33 @@ public class BallHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(!Touchscreen.current.primaryTouch.press.isPressed) {return;}
-
+        if(currentBallRigidbody == null) {return;}
+        if(!Touchscreen.current.primaryTouch.press.isPressed) 
+        {
+            if(isDraging)
+            {
+                LaunchBall();
+            }
+            isDraging = false;
+            return;
+        }
+        isDraging = true;
         Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
         Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
+        currentBallRigidbody.isKinematic = true;
+        currentBallRigidbody.position = worldPosition;
+    }
 
-        Debug.Log(worldPosition);
+    private void LaunchBall()
+    {
+        currentBallRigidbody.isKinematic = false;
+        currentBallRigidbody = null;
+        Invoke("DetachBall", delayDuration);
+      
+    }
+    private void DetachBall()
+    {
+        currentBallSpringJoint.enabled = false;
+        currentBallSpringJoint = null;
     }
 }
